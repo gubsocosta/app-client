eea3b9b7c932a0293a12e7eac271a1ab2bdff96e
Gabriel Costa <gubsocosta@gmail.com>
2023-09-30 16:47:00 -0300

    project initial

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AppClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
